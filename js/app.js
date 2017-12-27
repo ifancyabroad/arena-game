59eb7a918149c7ebc35a89cdccca16efc6bfa97e
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-12-27 18:18:15 +0000

    feat: Added portrait and class selection to the character creation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+const portraits = [
+	{
+		active: ko.observable(false),
+		path: 'images/female-warrior.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/male-mage.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/male-warrior.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/female-thief.jpg'
+	}
+]
+
+const classes = [
+	{
+		active: ko.observable(false),
+		name: 'Warrior',
+		description: 'Some text describing the warrior class'
+	},
+	{
+		active: ko.observable(false),
+		name: 'Mage',
+		description: 'Some text describing the mage class'
+	},
+	{
+		active: ko.observable(false),
+		name: 'Rogue',
+		description: 'Some text describing the rogue class'
+	}
+]
+
+
 const ViewModel = function() {
 	const self = this;
 	
@@ -6,9 +44,30 @@ const ViewModel = function() {
 	this.toggleCharacterCreation = function() {
 		if (self.characterCreationHidden() === true) {
 			self.characterCreationHidden(false);
-		}
-		else {
+		} else {
 			self.characterCreationHidden(true);
+		}
+	}
+	
+	this.togglePortraitSelection = function(portrait) {
+		portraits.forEach(function(p) {
+			p.active(false);
+		});
+		if (portrait.active() === false) {
+			portrait.active(true);
+		} else {
+			portrait.active(false);
+		}
+	}
+	
+	this.toggleClassSelection = function(cl) {
+		classes.forEach(function(c) {
+			c.active(false);
+		});
+		if (cl.active() === false) {
+			cl.active(true);
+		} else {
+			cl.active(false);
 		}
 	}
 }
