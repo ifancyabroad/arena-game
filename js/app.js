05a7378e6b06cbc3f3d3e0d552f32ab6afbaa43a
Edgar Nightingale <edgar.nightingale@btinternet.com>
2018-01-04 19:13:04 +0000

    feat: Player stats and skill points now update when leveling up
    Also added a lot more comments to the code so it is clearer and easier to understand.

--- js/app.js
+++ js/app.js
@@ -64,9 +64,13 @@ const enemies = [
 // Superclass for all Game Entities
 class GameEntity {
 	constructor(name, portrait, stats, armour = 0) {
+		const self = this;
+		
+		// Name and picture variables
 		this.name = ko.observable(name);
 		this.portrait = ko.observable(portrait);
 		
+		// Array to hold all player and enemy stats
 		this.stats = ko.observableArray([
 			{
 				name: 'Strength',
@@ -90,20 +94,24 @@ class GameEntity {
 			}
 		])
 		
-		const self = this;
+		// Set maximum health to constitution * 10
 		this.maxHealth = ko.computed(function() {
 			return self.stats()[2].value() * 10;
 		});
 		
+		// Initially set current health to maximum health
 		this.currentHealth = ko.observable(this.maxHealth());
 		
+		// Alive is true as long as current health is above 0
 		this.alive = ko.computed(function() {
 			return self.currentHealth() > 0; 
 		});
 		
+		// Armour variable
 		this.armour = ko.observable(armour);
 	}
 	
+	// Use dexterity stat to check whether or not attack hits
 	checkHit() {
 		if (this.stats()[1].value() >= getRandom(1, 20)) {
 			return true;
@@ -112,6 +120,7 @@ class GameEntity {
 		}
 	}
 	
+	// Mitigate physical damage based on armour stat
 	checkArmour(damage) {
 		if (damage - this.armour() < 0) {
 			return damage = 0;
@@ -120,14 +129,17 @@ class GameEntity {
 		}
 	}
 	
+	// Get physical damage based on strength stat
 	getPhysicalDamage() {		
 		return (this.stats()[0].value() + getRandom(1, 6));
 	}
 	
+	// Get magical damage based on intelligence
 	getMagicalDamage() {
 		return (this.stats()[3].value() + getRandom(1, 6));
 	}
 	
+	// Subtract from current health when hit
 	takeHit(damage) {
 		this.currentHealth(this.currentHealth() - damage);
 		return damage;
@@ -137,24 +149,35 @@ class GameEntity {
 // Subclass for the player
 class Player extends GameEntity {
 	constructor(name, portrait, cl, stats) {
+		
+		// Get name, portrait and stats from parent class
 		super(name, portrait, stats);
+		
+		// Variable for class selected by player
 		this.cl = ko.observable(cl);
+		
+		// Variable for experience obtained by player
 		this.experience = ko.observable(0);
+		
+		// Player level, starts at 1
 		this.level = ko.observable(1);
+		
+		// Variable for rerolls the player is allowed
 		this.rerolls = ko.observable(3);
+		
+		// Skill points the player has acquired for spending
 		this.skillPoints = ko.observable(5);
-		const self = this;
 	}
 	
-	updateStat(stat, n) {
-		if (((this.skillPoints() - n) >= 0) && ((stat.value() + n) <= 20)) {
-			stat.value(stat.value() + n);
-			this.skillPoints(this.skillPoints() - n)
+	// Update the players stats based on an array of stats taken as a parameter
+	updateStats(newStats) {
+		for (let i = 0; i < this.stats().length; i++) {
+			this.stats()[i].value(newStats[i].value());			
 		}
 	}
 }
 
-// Get random number within a range
+// Get random number within a specified range
 const getRandom = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -175,18 +198,20 @@ const ViewModel = function() {
 	this.playerCardShow = ko.observable(false);
 	this.enemyCardShow = ko.observable(false);
 	
-	// Toggle visibility of start game screen and character creation
+	// Flip the UI card to toggle visibility of start game screen and character creation
 	this.toggleStartGame = function() {
 		self.uiCardShow(true);
 	}
 	
-	// Toggle visibility of character creation screen and roll stats
+	// Switch display from character creation screen to roll stats
 	this.toggleRollStats = function() {
 		self.characterCreationShow(false);
 		self.rollStatsShow(true);
 	}
 	
-	// Toggle visibility of roll stats and town screen
+	// Flip player card to reveal character
+	// Switch the start game screen with the town screen
+	// Flip the UI card
 	this.toggleTown = function() {
 		self.playerCardShow(true);
 		self.startGameShow(false);
@@ -194,44 +219,85 @@ const ViewModel = function() {
 		self.uiCardShow(false);
 	}
 	
-	// Toggle visibility of town screen and battle screen
+	// Flip the UI card to reveal the battle screen
 	this.toggleBattle = function() {
+		
+		// If battle screen is not visible, make it visible and hide other screens
 		if (self.battleShow() === false) {
 			self.rollStatsShow(false);
 			self.levelUpShow(false);
 			self.battleShow(true);
 		}
+		
+		// Reset battle log
 		self.battleLog('');
+		
+		// Get new enemy and flip the enemy card to reveal it
 		self.getEnemy();
 		setTimeout(function() { 
 			self.enemyCardShow(true)
 		}, 300);
+		
+		// Flip the UI card
 		self.uiCardShow(true);
 	}
 	
-	// Toggle visibility of battle screen and town screen
+	// Return to town after battle by flipping enemy and UI cards
 	this.toggleReturn = function() {
 		self.enemyCardShow(false);
 		self.uiCardShow(false);
 	}
 	
+	// Set variables for initial stats and skill points when entering level up screen
+	this.tempStats = ko.observableArray();
+	this.tempSkillPoints;
+	
 	// Toggle visibility of town screen and level up screen
 	this.toggleLevelUp = function() {
+		
+		// Set tempStats and tempSkillPoints to current player stats and skill points
+		self.tempStats(self.player().stats().map(function(stat) {
+			return {name: stat.name, value: ko.observable(stat.value()), min: stat.value()} 
+		}));
+		self.tempSkillPoints = self.player().skillPoints();
+		
+		// Set visibility of levelup screen and flip UI card
 		self.rollStatsShow(false);
 		self.battleShow(false);
 		self.levelUpShow(true);
 		self.uiCardShow(true);
 	}
 	
+	// Change stats on levelup screen
+	this.changeStat = function(n) {	
+
+		// Check placing or subtracting the point will not be higher than 20 or lower than initial value
+		if (((self.player().skillPoints() - n) >= 0) && ((this.value() + n) >= this.min) && ((this.value() + n) <= 20)) {
+			
+			// Increment/decrement the stat and add or remove a skill point
+			this.value(this.value() + n);
+			self.player().skillPoints(self.player().skillPoints() - n)
+		}
+	}
+	
 	// Return from level up screen
 	this.toggleLevelUpReturn = function() {
+		// Update player stats with new values and update player level based on points spent
+		self.player().updateStats(self.tempStats());
+		self.player().level(self.player().level() + (self.tempSkillPoints - self.player().skillPoints()));
+		
+		// Flip UI card
 		self.uiCardShow(false);
 	}
 	
-	// Toggle browse right through portraits
+	// Toggle browse through portraits
 	this.togglePortrait = function(n) {
+		
+		// Variable for next active portrait
 		let currentIndex;
 		
+		// Set all portraits to not active
+		// Set currentIndex to the next portrait
 		portraits.forEach(function(portrait) {
 			if (portrait.active() === true) {
 				portrait.active(false);
@@ -239,20 +305,26 @@ const ViewModel = function() {
 			}
 		});
 		
+		// Ensure the next portrait loops around if it is the first or last in the array
 		if (currentIndex >= portraits.length) {
 			currentIndex = 0;
 		} else if (currentIndex < 0) {
 			currentIndex = portraits.length - 1;
 		}
 		
+		// Set next portrait to the active one
 		portraits[currentIndex].active(true);
 	}
 	
 	// Toggle selection of classes
 	this.toggleClassSelection = function(cl) {
+		
+		// Set all classes to not selected
 		classes.forEach(function(c) {
 			c.active(false);
 		});
+		
+		// Activate or de-activate selected class
 		if (cl.active() === false) {
 			cl.active(true);
 		} else {
@@ -266,14 +338,20 @@ const ViewModel = function() {
 	
 	// Create character with selected options once form is submitted
 	this.createCharacter = function() {
+		
+		// Variables for selected character features
 		let portrait;
 		let cl;
 		let playerStats = [];
+		
+		// Find selected portrait
 		portraits.forEach(function(p) {
 			if (p.active() === true) {
 				portrait = p.path;
 			}			
 		});
+		
+		// Find selected class and set stats based on the class selected
 		classes.forEach(function(c) {
 			if (c.active() === true) {
 				cl = c;
@@ -282,32 +360,30 @@ const ViewModel = function() {
 				});
 			}
 		});
-		
-		
+			
+		// Create player instance and reveal roll stats screen
 		self.player(new Player(self.nameInput(), portrait, cl, playerStats));
 		self.toggleRollStats();
 	}
 	
 	// Create and roll for player stats
 	this.rollStats = function() {
+		
+		// Find maximum stats for selected class
 		let classStats = self.player().cl().maxStats;
 		
+		// Check there are rerolls remaining
 		if (self.player().rerolls() > 0) {
+			
+			// Set each stat as a number up to 5 less than the class maximum
 			for (let i = 0; i < self.player().stats().length; i++) {
 				self.player().stats()[i].value(getRandom(classStats[i] - 5, classStats[i]));
 			}
+			
+			// Set current health and subtract one from rerolls
 			self.player().currentHealth(self.player().maxHealth());
 			self.player().rerolls(self.player().rerolls() - 1)
 		}
-	}
-	
-	this.changeStat = function(n) {
-		self.player().updateStat(this, n);
-	}
-	
-	// Level up
-	this.levelUp = function() {
-		
 	}
 	
 	// Create current enemy variable
@@ -325,10 +401,18 @@ const ViewModel = function() {
 	// Check to see if the player hits, calculate the damage and display it in the combat log
 	this.playerAttack = function() {
 		if (self.player().checkHit()) {
+			
+			// Get player damage
 			let playerDmg = self.currentEnemy().checkArmour(self.player().getPhysicalDamage());
+			
+			// Enemy takes player damage
 			self.currentEnemy().takeHit(playerDmg);
+			
+			// Display in battle log
 			self.battleLog(self.battleLog() + `<p>You attack the ${self.currentEnemy().name()} for ${playerDmg} damage</p>`);
 		} else {
+			
+			// Log a miss in the battle log
 			self.battleLog(self.battleLog() + `<p>You miss the ${self.currentEnemy().name()}</p>`);
 		}
 	}
@@ -342,14 +426,23 @@ const ViewModel = function() {
 	// Check to see if the enemy hits, calculate the damage and display it in the combat log
 	this.enemyAttack = function() {
 		if (self.currentEnemy().checkHit()) {
+			
+			// Get enemy damage
 			let enemyDmg = self.player().checkArmour(self.currentEnemy().getPhysicalDamage());
+			
+			// Player takes enemy damage
 			self.player().takeHit(enemyDmg);
+			
+			// Display in battle log
 			self.battleLog(self.battleLog() + `<p>${self.currentEnemy().name()} attacks you for ${enemyDmg} damage</p>`);
 		} else {
+			
+			// Log a miss in the battle log
 			self.battleLog(self.battleLog() + `<p>${self.currentEnemy().name()} misses you</p>`);
 		}
 	}
 	
+	// Log enemy death in battle log
 	this.enemySlain = function() {
 		self.battleLog(self.battleLog() + `<p>You have slain the ${self.currentEnemy().name()}!</p>`);
 	}
@@ -359,9 +452,13 @@ const ViewModel = function() {
 		
 		// Check who strikes first based on initiative
 		if (self.player().stats()[4].value() >= self.currentEnemy().stats()[4].value()) {
+			
+			// Attack and check for enemy death, if alive enemy retaliates
 			self.playerAttack();
 			self.currentEnemy().alive() ? self.enemyAttack() : self.enemySlain();
 		} else {
+			
+			// Enemy attacks first followed by player and check for enemy death
 			self.enemyAttack();
 			self.playerAttack();
 			if (!self.currentEnemy().alive()) {
@@ -375,9 +472,13 @@ const ViewModel = function() {
 		
 		// Check who strikes first based on initiative
 		if (self.player().stats()[4].value() >= self.currentEnemy().stats()[4].value()) {
+			
+			// Attack and check for enemy death, if alive enemy retaliates
 			self.playerMagicAttack();
 			self.currentEnemy().alive() ? self.enemyAttack() : self.enemySlain();
 		} else {
+			
+			// Enemy attacks first followed by player and check for enemy death
 			self.enemyAttack();
 			self.playerMagicAttack();
 			if (!self.currentEnemy().alive()) {
@@ -388,22 +489,36 @@ const ViewModel = function() {
 	
 	// Reset the game back to its starting state
 	this.resetGame = function() {
+		
+		// Hide player and enemy cards
 		self.playerCardShow(false);
 		self.enemyCardShow(false);
+		
+		// Set player and enemy objects to null
 		self.player(null);
 		self.currentEnemy(null);
+		
+		// Hide town and battle screens, show start game and character creation
 		self.townShow(false);
 		self.battleShow(false);
 		self.startGameShow(true);
 		self.characterCreationShow(true);
+		
+		// Flip UI card
 		self.uiCardShow(false);
+		
+		// Reset portraits
 		portraits.forEach(function(p) {
 			p.active(false);
 		});
 		portraits[0].active(true);
+		
+		// Reset classes
 		classes.forEach(function(c) {
 			c.active(false);
 		});
+		
+		// Reset player name
 		self.nameInput('');
 	}
 }
