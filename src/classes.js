357d9cfbfe944b48ec8ba96fcae353e7c6caba9b
Edgar Nightingale <edgar.nightingale@btinternet.com>
2018-01-24 18:56:37 +0000

    refactor: Separated files for better management

---
+++ src/classes.js
@@ -0,0 +1,203 @@
+// Superclass for all Game Entities
+class GameEntity {
+	constructor(name, portrait, stats, armour = 0) {
+		const self = this;
+		
+		// Name and picture variables
+		this.name = ko.observable(name);
+		this.portrait = ko.observable(portrait);
+		
+		// Array to hold all player and enemy stats
+		this.stats = ko.observableArray([
+			{
+				name: 'Strength',
+				value: ko.observable(stats[0])
+			},
+			{
+				name: 'Dexterity',
+				value: ko.observable(stats[1])
+			},
+			{
+				name: 'Constitution',
+				value: ko.observable(stats[2])
+			},
+			{
+				name: 'Intelligence',
+				value: ko.observable(stats[3])
+			},
+			{
+				name: 'Initiative',
+				value: ko.observable(stats[4])
+			}
+		])
+		
+		// Set maximum health to constitution * 10
+		this.maxHealth = ko.computed(function() {
+			return self.stats()[2].value() * 10;
+		});
+		
+		// Initially set current health to maximum health
+		this.currentHealth = ko.observable(this.maxHealth());
+		
+		// Alive is true as long as current health is above 0
+		this.alive = ko.computed(function() {
+			return self.currentHealth() > 0; 
+		});
+		
+		// Stat modifiers
+		this.modifiers = [0, 0, 0, 0, 0];
+		
+		// Armour variable
+		this.armour = armour;
+	}
+	
+	// Use dexterity stat to check whether or not attack hits
+	checkHit() {
+		let dex = this.stats()[1].value() + this.modifiers[1];
+		if (dex >= getRandom(1, 20)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Mitigate physical damage based on armour stat
+	checkArmour(damage) {
+		if (damage - this.armour < 0) {
+			return damage = 0;
+		} else {
+			return damage - this.armour;
+		}
+	}
+	
+	// Mitigate magical damage based on intelligence stat
+	checkIntelligence(damage) {
+		let intel = this.stats()[3].value() + this.modifiers[3];
+		if (damage - intel < 0) {
+			return damage = 0;
+		} else {
+			return damage - intel;
+		}
+	}
+	
+	// Get physical damage based on strength stat
+	getPhysicalDamage() {	
+		let str = this.stats()[0].value() + this.modifiers[0];
+		return (str + getRandom(1, 6));
+	}
+	
+	// Get magical damage based on intelligence
+	getMagicalDamage() {
+		let intel = this.stats()[3].value() + this.modifiers[3];
+		return (intel + getRandom(1, 6));
+	}
+	
+	// Subtract from current health when hit
+	takeHit(damage) {
+		this.currentHealth(this.currentHealth() - damage);
+		return damage;
+	}
+}
+
+// Subclass for the player
+class Player extends GameEntity {
+	constructor(name, portrait, cl, stats) {
+		
+		// Get name, portrait and stats from parent class
+		super(name, portrait, stats);
+		
+		const self = this;
+		
+		// Variable for class selected by player
+		this.cl = ko.observable(cl);
+		
+		// Variable for experience obtained by player
+		this.experience = ko.observable(0);
+		
+		// Variable for how much gold the player is holding
+		this.gold = ko.observable(0);
+		
+		// Experience chart
+		this.levelTier = ko.computed(function() {
+			if (self.experience() > 20000) {
+				return 10;
+			} else if (self.experience() > 12000) {
+				return 9;
+			} else if (self.experience() > 8000) {
+				return 8;
+			} else if (self.experience() > 5000) {
+				return 7;
+			} else if (self.experience() > 3000) {
+				return 6;
+			} else if (self.experience() > 1500) {
+				return 5;
+			} else if (self.experience() > 800) {
+				return 4;
+			} else if (self.experience() > 300) {
+				return 3;
+			} else if (self.experience() > 100) {
+				return 2;
+			} else {
+				return 1;
+			}
+		});
+		
+		// Player level, starts at 1
+		this.level = ko.observable(1);
+		
+		// Variable for rerolls the player is allowed
+		this.rerolls = ko.observable(3);
+		
+		// Skill points the player has acquired for spending
+		this.skillPoints = ko.observable(0);
+		
+		// Inventory
+		this.inventory = ko.observable(
+			{
+				head: ko.observable({ name: 'None' }),
+				body: ko.observable({ name: 'None'  }),
+				gloves: ko.observable({ name: 'None' }),
+				boots: ko.observable({ name: 'None' }),
+				weapon: ko.observable({ name: 'None' }),
+				misc: ko.observable({ name: 'None' })
+			});
+	}
+	
+	// Update the players stats based on an array of stats taken as a parameter
+	updateStats(newStats) {
+		for (let i = 0; i < this.stats().length; i++) {
+			this.stats()[i].value(newStats[i].value());			
+		}
+	}
+	
+	// Increase experience
+	experienceGain(xp) {
+		let currentTier = this.levelTier();
+		this.experience(this.experience() + xp);
+		if (currentTier !== this.levelTier()) {
+			this.skillPoints(this.skillPoints() + 1);
+		}
+	}
+}
+
+// Subclass for the enemy
+class Enemy extends GameEntity {
+	constructor(name, portrait, stats, armour = 0, expValue, goldValue) {
+		
+		// Get name, portrait and stats from parent class
+		super(name, portrait, stats, armour);
+		
+		const self = this;
+		
+		// Variable for how much experience the enemy is worth
+		this.expValue = expValue;
+		
+		// Variable for how much gold the enemy is worth
+		this.goldValue = goldValue;
+	}
+}
+
+// Get random number within a specified range
+const getRandom = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
---
+++ src/model.js
@@ -0,0 +1,274 @@
+// Array of portraits that can be found in the images folder
+const portraits = [
+	{
+		active: ko.observable(true),
+		path: 'images/female-warrior.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/male-mage.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/male-warrior.jpg'
+	},
+	{
+		active: ko.observable(false),
+		path: 'images/female-thief.jpg'
+	}
+]
+
+// Array of possible classes to choose from
+const classes = [
+	{
+		active: ko.observable(true),
+		name: 'Warrior',
+		description: 'Some text describing the warrior class',
+		maxStats: [15, 10, 13, 6, 10]
+	},
+	{
+		active: ko.observable(false),
+		name: 'Mage',
+		description: 'Some text describing the mage class',
+		maxStats: [6, 9, 10, 15, 10]
+	},
+	{
+		active: ko.observable(false),
+		name: 'Rogue',
+		description: 'Some text describing the rogue class',
+		maxStats: [9, 15, 10, 9, 12]
+	}
+]
+
+// Array of possible enemies from tier 1
+const enemiesOne = [
+	{
+		name: 'Goblin',
+		portrait: 'images/goblin.jpg',
+		stats: [5, 7, 3, 2, 6],
+		armour: 2,
+		expValue: 50,
+		goldValue: 50
+	},
+	{
+		name: 'Kobold',
+		portrait: 'images/kobold.jpg',
+		stats: [4, 8, 3, 2, 6],
+		armour: 3,
+		expValue: 50,
+		goldValue: 50
+	},
+	{
+		name: 'Wolf',
+		portrait: 'images/wolf.jpg',
+		stats: [6, 8, 4, 1, 7],
+		expValue: 75,
+		goldValue: 50
+	},
+	{
+		name: 'Orc',
+		portrait: 'images/orc.jpg',
+		stats: [8, 7, 7, 3, 7],
+		armour: 8,
+		expValue: 150,
+		goldValue: 100
+	},
+	{
+		name: 'Imp',
+		portrait: 'images/imp.jpg',
+		stats: [2, 5, 3, 10, 8],
+		expValue: 50,
+		goldValue: 50
+	}
+]
+
+// Array of possible enemies from tier 2
+const enemiesTwo = [
+	{
+		name: 'Orc',
+		portrait: 'images/orc.jpg',
+		stats: [10, 7, 9, 3, 7],
+		armour: 8,
+		expValue: 150,
+		goldValue: 100
+	},
+	{
+		name: 'Troll',
+		portrait: 'images/troll.jpg',
+		stats: [12, 5, 13, 2, 5],
+		armour: 2,
+		expValue: 200,
+		goldValue: 150
+	},
+	{
+		name: 'Gauth',
+		portrait: 'images/gauth.png',
+		stats: [5, 7, 8, 12, 10],
+		armour: 4,
+		expValue: 150,
+		goldValue: 100
+	},
+	{
+		name: 'Mage',
+		portrait: 'images/mage.jpg',
+		stats: [3, 5, 7, 15, 11],
+		armour: 2,
+		expValue: 200,
+		goldValue: 150
+	},
+	{
+		name: 'Mind Flayer',
+		portrait: 'images/flayer.jpg',
+		stats: [7, 11, 10, 16, 12],
+		armour: 4,
+		expValue: 400,
+		goldValue: 300
+	}
+]
+
+// Array of possible enemies from tier 3
+const enemiesThree = [
+	{
+		name: 'Mind Flayer',
+		portrait: 'images/flayer.jpg',
+		stats: [6, 8, 10, 16, 12],
+		armour: 4,
+		expValue: 400,
+		goldValue: 300
+	},
+	{
+		name: 'Golem',
+		portrait: 'images/golem.jpg',
+		stats: [14, 9, 17, 12, 11],
+		armour: 12,
+		expValue: 750,
+		goldValue: 500
+	},
+	{
+		name: 'Lich',
+		portrait: 'images/lich.jpg',
+		stats: [8, 9, 10, 18, 15],
+		armour: 4,
+		expValue: 1000,
+		goldValue: 750
+	},
+	{
+		name: 'Werewolf',
+		portrait: 'images/werewolf.png',
+		stats: [12, 15, 11, 10, 16],
+		expValue: 500,
+		goldValue: 400
+	},
+	{
+		name: 'Dragon',
+		portrait: 'images/dragon.jpg',
+		stats: [17, 16, 18, 19, 16],
+		armour: 14,
+		expValue: 2000,
+		goldValue: 1500
+	}
+]
+
+// Array of available items for the shop
+const items = ko.observableArray([
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'head',
+		name: 'Steel Helm',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'armour',
+		value: 2,
+		price: 200
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'head',
+		name: 'Magic Helm',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'intelligence',
+		value: 2,
+		price: 300
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'body',
+		name: 'Chainmail',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'armour',
+		value: 4,
+		price: 250
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'body',
+		name: 'Plate Armour',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'armour',
+		value: 6,
+		price: 400
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'weapon',
+		name: 'Longsword',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'strength',
+		value: 2,
+		price: 100
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'weapon',
+		name: 'Greataxe',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'strength',
+		value: 4,
+		price: 300
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'weapon',
+		name: 'Magic Staff',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'intelligence',
+		value: 3,
+		price: 300
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'boots',
+		name: 'Boots of Speed',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'initiative',
+		value: 2,
+		price: 100
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'gloves',
+		name: 'Gloves of Accuracy',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'dexterity',
+		value: 2,
+		price: 200
+	},
+	{
+		active: ko.observable(false),
+		available: ko.observable(true),
+		type: 'misc',
+		name: 'Ring of Swiftness',
+		description: function() { return `Increases your ${this.modifier} by ${this.value}` },
+		modifier: 'initiative',
+		value: 3,
+		price: 200
+	}
+]);
